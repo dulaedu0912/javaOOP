cf92a8c51db32cf613fecf2f95749ec2fe5bc6ca
dulaedu0912 <dulaedu0912@gmail.com>
2026-06-19 14:09:56 +0530

    first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("dulara", 23);
+
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+    }
+}
